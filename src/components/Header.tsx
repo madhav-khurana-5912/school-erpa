185e096e12bf442b703617e73a4c63a9ba624c69
maddykk 812 <maddykk812@gmail.com>
2025-07-12 18:06:31 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarTrigger>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from 'react';
-import { SidebarTrigger } from "@/components/ui/sidebar";
 import { useSidebar } from "@/components/ui/sidebar";
 
 type HeaderProps = {
@@ -14,7 +13,6 @@ export function Header({ title, children }: HeaderProps) {
   
   return (
     <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-      {isMobile && <SidebarTrigger />}
       <h1 className="text-xl font-semibold">{title}</h1>
       <div className="ml-auto flex items-center gap-2">
         {children}
